6eabc8b288062888c090997df393479744556def
procham <procham1@proton.me>
2024-09-19 23:08:48 +0200

    Added DMing users
    You can now DM users with the bot. Use ccb open_dm <user_id> to open a DM channel in the DM category.
    Everything you send in the the channel is sent in DM to the user, and vice-versa.

--- cogs/announcements.py
+++ cogs/announcements.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
 
+import os
+from dotenv import load_dotenv
+
+import json
+
+load_dotenv()
+
+
 class Announcements(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -9,6 +17,51 @@ class Announcements(commands.Cog):
     async def say(self, ctx, *, what):
         await ctx.send(what)
         await ctx.message.delete()
+
+    @commands.command()
+    async def open_dm(self, ctx, user_id):
+        user = await self.bot.fetch_user(user_id)
+        guild = ctx.message.guild
+        channel = await guild.create_text_channel(name=user.name, category=self.bot.get_channel(int(os.getenv("DMS_CATEGORY"))))
+        try:
+            with open("dms.json", "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            data = {}
+        data[user.id] = channel.id
+        data[channel.id] = user.id
+        with open("dms.json", "w") as f:
+            json.dump(data, f, indent=4)
+        await ctx.send(f"Salon privé créé!")
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        guild = message.guild
+        category = self.bot.get_channel(int(os.getenv("DMS_CATEGORY")))
+        if message.guild and message.channel.category != category:
+            return
+        with open("dms.json", "r") as f:
+            data = json.load(f)
+        
+        #If the message is sent from the server
+        if message.guild and message.channel.category == category:
+            user = discord.utils.get(guild.members, id=data[str(message.channel.id)])
+            dm = await user.create_dm()
+            if message.content and message.content.startswith("_") == False:
+                await dm.send(message.content)
+            if message.attachments:
+                for attachment in message.attachments:
+                    await dm.send(attachment.url)
+        #If the message is sent from the DM
+        if message.guild == None:
+            channel_to_send = self.bot.get_channel(data[str((message.author.id))])
+            if message.content:
+                await channel_to_send.send(message.content)
+            if message.attachments:
+                for attachment in message.attachments:
+                    await channel_to_send.send(attachment.url)
 
     async def cog_load(self):
         print(f"{self.__class__.__name__} loaded!")
